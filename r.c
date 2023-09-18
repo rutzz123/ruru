74c51f15f243c88ec687c1eee108c7545e453d95
rutzz123 <rutvakalathia.com>
2023-09-18 16:55:59 +0530

    my new repo

---
+++ r.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter your number");
+   scanf("%d",&a);
+   
+       a%2==0?printf("the number is even"): printf("this number is odd");
+       
+    return 0;
+}
